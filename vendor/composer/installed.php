14bc00fae6a8d68c1762eb6dee12beb6a7a9844f
PATEL NIDHI SHARAD <nipa006@ess.ais.ac.nz>
2023-07-07 12:02:12 +1200

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'phpmailer/phpmailer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5f18f6123cc9e36c8c32c52b9d224e5b0fa7e7a2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5f18f6123cc9e36c8c32c52b9d224e5b0fa7e7a2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.7.2',
+            'version' => '3.7.2.0',
+            'reference' => 'ed8e00df0a83aa96acf703f8c2979ff33341f879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
