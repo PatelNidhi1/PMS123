2fc891b33508c72eadb42bc8f6bc84ff5913bd3e
PATEL NIDHI SHARAD <nipa006@ess.ais.ac.nz>
2023-07-08 15:17:43 +1200

    This is a new account commit

--- dbConfig.php
+++ dbConfig.php
@@ -13,3 +13,8 @@ $db = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
 if ($db->connect_error) { 
     die("Connection failed: " . $db->connect_error); 
 }
+
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "pmss.mysql.database.azure.com", "nipa006", "{Jkdrjpwxgm9\}", "{pms}", 3306, MYSQLI_CLIENT_SSL);
--- db_connect.php
+++ db_connect.php
@@ -1,3 +1,4 @@
 <?php 
 
 $conn= new mysqli('localhost','root','','pms')or die("Could not connect to mysql".mysqli_error($con));
+
